cec5c69d7726350a4e779558abf676a1d2ab745e
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2024-12-03 10:52:43 +0100

    Update intel-nuc-video-drivers.sh

--- intel-nuc-video-drivers.sh
+++ intel-nuc-video-drivers.sh
@@ -6,7 +6,5 @@ sudo pacman -S --needed vulkan-intel --noconfirm
 sudo pacman -S --needed lib32-vulkan-intel --noconfirm
 sudo pacman -S --needed vulkan-icd-loader --noconfirm
 sudo pacman -S --needed lib32-vulkan-icd-loader --noconfirm
-sudo pacman -S --needed libva-mesa-driver --noconfirm
-sudo pacman -S --needed lib32-libva-mesa-driver --noconfirm
 
 echo 'A TELEPÍTÉS BEFEJEZŐDÖTT, KÉREM, HOGY INDÍTSA ÚJRA A SZÁMÍTÓGÉPÉT!'
